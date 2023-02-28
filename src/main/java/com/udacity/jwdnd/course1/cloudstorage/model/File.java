9a8338ad86b200a16d8966ac7b977379e61e8319
May Lin <40640630+maymlin@users.noreply.github.com>
2023-02-28 14:28:10 -0600

    Create File Model

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/File.java
@@ -0,0 +1,32 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class File {
+
+    private Integer fileId;
+    private String fileName;
+    private Integer userId;
+
+    public Integer getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(Integer fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
@@ -3,14 +3,14 @@ package com.udacity.jwdnd.course1.cloudstorage.model;
 
 public class User {
 
-    private String userId;
+    private Integer userId;
     private String username;
     private String salt;
     private String password;
     private String firstName;
     private String lastName;
 
-    public User(String userId, String username, String salt, String password, String firstName, String lastName) {
+    public User(Integer userId, String username, String salt, String password, String firstName, String lastName) {
         this.userId = userId;
         this.username = username;
         this.salt = salt;
@@ -19,11 +19,11 @@ public class User {
         this.lastName = lastName;
     }
 
-    public String getUserId() {
+    public Integer getUserId() {
         return userId;
     }
 
-    public void setUserId(String userId) {
+    public void setUserId(Integer userId) {
         this.userId = userId;
     }
 
