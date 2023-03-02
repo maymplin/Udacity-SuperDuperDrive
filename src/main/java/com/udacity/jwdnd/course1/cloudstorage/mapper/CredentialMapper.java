390321967da63888a4123fc31fac79538f3bc6f2
May Lin <40640630+maymlin@users.noreply.github.com>
2023-03-01 22:04:58 -0600

    Create CredentialMapper. Remove field key from Credential model.

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialMapper.java
@@ -0,0 +1,31 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface CredentialMapper {
+
+    @Select("SELECT * FROM credentials WHERE userid = #{userId}")
+    List<Credential> getCredentialsByUserId(Integer userId);
+
+    @Select("SELECT * FROM credentials WHERE credentialid = #{credentialId}")
+    Credential getCredentialByCredentialId(Integer credentialId);
+
+    @Insert("INSERT INTO credentials (url, username, password, userid) " +
+            "VALUES(#{url}, #{username}, #{password}, #{userId})")
+    @Options(useGeneratedKeys = true, keyProperty = "credentialId")
+    int insert(Credential credential);
+
+    @Delete("DELETE FROM credentials WHERE credentialid = #{credentialId}")
+    void delete(Integer credentialId);
+
+    @Update("UPDATE credentials SET url = #{url}, " +
+            "username = #{username} " +
+            "password = #{password}" +
+            "WHERE userid = #{userId}")
+    void update(Credential credential);
+}
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Credential.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Credential.java
@@ -5,7 +5,6 @@ public class Credential {
     private Integer credentialId;
     private String url;
     private String username;
-    private String key;
     private String password;
     private Integer userId;
 
@@ -31,14 +30,6 @@ public class Credential {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
     }
 
     public String getPassword() {
