6df3c5b2c8876a6ce01d599655a734ebb2ec9ccd
May Lin <40640630+maymlin@users.noreply.github.com>
2023-03-02 22:59:22 -0600

    Implement tests for note creation, viewing, editing, and deletion

---
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
@@ -0,0 +1,80 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HomePage {
+
+    @FindBy(id = "nav-notes-tab")
+    private WebElement notesTabButton;
+    @FindBy(id = "edit-note-button")
+    private WebElement editNoteButton;
+    @FindBy(id = "delete-note")
+    private WebElement deleteNoteButton;
+    @FindBy(id = "note-title")
+    private WebElement noteTitleField;
+    @FindBy(id = "note-description")
+    private WebElement noteDescriptionField;
+    @FindBy(id = "nav-credentials-tab")
+    private WebElement credentialsTabButton;
+
+    private WebDriver driver;
+    private WebDriverWait webDriverWait;
+
+    public HomePage(WebDriver driver, WebDriverWait webDriverWait) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        this.webDriverWait = webDriverWait;
+    }
+
+    public void createNote(String noteTitle, String noteDescription) {
+        this.notesTabButton.click();
+
+        this.webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id("add-note")));
+//        this.addNoteButton = driver.findElement(By.id("add-note"));
+        driver.findElement(By.id("add-note")).click();
+        this.webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id("create-note-button")));
+//        this.noteTitleField = driver.findElement(By.id("note-title"));
+        this.noteTitleField.sendKeys(noteTitle);
+//        this.noteDescriptionField = driver.findElement(By.id("note-description"));
+        this.noteDescriptionField.sendKeys(noteDescription);
+        driver.findElement(By.id("create-note-button")).click();
+
+    }
+
+    public String retrieveNoteTitle() {
+        this.notesTabButton.click();
+        this.webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id("add-note")));
+
+        return this.driver.findElement(By.className("noteTitle")).getText();
+    }
+
+    public void editNoteTitle(String newNoteTitle) {
+        this.notesTabButton.click();
+        this.webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id("add-note")));
+        this.editNoteButton.click();
+        this.webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id("create-note-button")));
+        this.noteTitleField.clear();
+        this.noteTitleField.sendKeys(newNoteTitle);
+        this.driver.findElement(By.id("create-note-button")).click();
+    }
+
+    public void deleteNote() {
+        this.notesTabButton.click();
+        this.webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id("add-note")));
+        this.driver.findElement(By.id("delete-note")).click();
+    }
+
+    public Boolean isNoteDisplayed() {
+        this.notesTabButton.click();
+        this.webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id("add-note")));
+
+        return this.driver.findElement(By.id("note-title")).isDisplayed();
+
+    }
+}
--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/NotesAndCredentialsTest.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/NotesAndCredentialsTest.java
@@ -2,7 +2,6 @@ package com.udacity.jwdnd.course1.cloudstorage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -11,17 +10,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class SignupAndLoginFlowTest {
+public class NotesAndCredentialsTest {
 
     @LocalServerPort
     private int port;
     private static WebDriver driver;
     private static WebDriverWait webDriverWait;
-    private static final String baseUrl = "http://localhost:";
-    private static final String firstName = "Bertrand";
-    private static final String lastName = "Russell";
-    private static final String username = "brussell";
-    private static final String password = "password";
+
+    private static HomePage homePage;
+    private static String baseUrl = "http://localhost:";
+    private static final String firstName = "Godfrey";
+    private static final String lastName = "Hardy";
+    private static final String username = "ghhardy";
+    private static final String password = "test123";
 
     @BeforeAll
     public static void beforeAll() {
@@ -31,6 +32,9 @@ public class SignupAndLoginFlowTest {
     @BeforeEach
     public void beforeEach() {
         driver = new ChromeDriver();
+        webDriverWait = new WebDriverWait(driver, 3);
+        homePage = new HomePage(driver, webDriverWait);
+        baseUrl += port;
     }
 
     @AfterEach
@@ -49,47 +53,39 @@ public class SignupAndLoginFlowTest {
     }
 
     @Test
-    public void signupLoginHomeAccessTest() {
-        webDriverWait = new WebDriverWait(driver, 3);
+    public void noteCreationEditDeleteTest() {
 
-        // Sign-up new user
-        driver.get(baseUrl + this.port + "/signup");
+        // Sign-up and log-in
+        driver.get(baseUrl + "/signup");
         webDriverWait.until(ExpectedConditions.titleContains("Sign Up"));
-
         SignupPage signupPage = new SignupPage(driver);
         doMockSignup(firstName, lastName, username, password);
 
-        Assertions.assertTrue(signupPage.isSignupSuccessful());
-
-        // Log in
-        driver.get(baseUrl + this.port + "/login");
-        webDriverWait.until(ExpectedConditions.titleContains("Login"));
-
-        doMockLogin(username, password);
-
-        Assertions.assertEquals("Home", driver.getTitle());
-    }
-
-    @Test
-    public void unauthorizedAccessTest() {
-        driver.get(baseUrl + this.port + "/signup");
-        doMockSignup(firstName, lastName, username, password);
-        webDriverWait = new WebDriverWait(driver, 3);
-        driver.get(baseUrl + this.port + "/login");
+        driver.get(baseUrl + "/login");
         webDriverWait.until(ExpectedConditions.titleContains("Login"));
         doMockLogin(username, password);
 
-        // Log out
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.logout();
+        webDriverWait.until(ExpectedConditions.titleContains("Home"));
 
-        // Verify user is logged out
-        Assertions.assertEquals(baseUrl + this.port + "/login", driver.getCurrentUrl());
+        // Create note
+        homePage.createNote("Test", "Hello world!");
 
-        // Try to access home page
-        driver.get(baseUrl + this.port + "/home");
+        webDriverWait.until(ExpectedConditions.titleContains("Home"));
 
-        // Should be redirected to login page
-        Assertions.assertEquals(baseUrl + this.port + "/login", driver.getCurrentUrl());
+        // Verify note is visible
+        Assertions.assertEquals("Test", homePage.retrieveNoteTitle());
+        webDriverWait.until(ExpectedConditions.titleContains("Home"));
+
+        // Edit note by changing its title
+        homePage.editNoteTitle("Edited Test");
+
+        // Verify note is edited
+        Assertions.assertEquals("Edited Test", homePage.retrieveNoteTitle());
+
+        // Delete note
+        homePage.deleteNote();
+
+        // Verify note is deleted by checking that it is no longer displayed
+        Assertions.assertFalse(homePage.isNoteDisplayed());
     }
 }
