67544819ee11505651a5bc5b4b8815c21f35dbf7
May Lin <40640630+maymlin@users.noreply.github.com>
2023-03-04 14:38:16 -0600

    Add key field to Credential model

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Credential.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Credential.java
@@ -5,6 +5,7 @@ public class Credential {
     private Integer credentialId;
     private String url;
     private String username;
+    private String key;
     private String password;
     private Integer userId;
 
@@ -30,6 +31,14 @@ public class Credential {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
     }
 
     public String getPassword() {
